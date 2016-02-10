e1351b7d277de2c283a70a2c4b0c03cea4c552cd
Alyson <emerald.aly@gmail.com>
2016-02-09 19:07:07 -0500

    calculator functions

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express = require('express');
-var omdb = require('omdb');
-var github = require('github');
-var GitHubApi = require('node-github');
 
 var app = express();
 var PORT = process.env.PORT || 8090;
+
+app.get('/addition/:a/:b', function(req, res){
+  res.send(JSON.stringify(parseInt(req.params.a) + parseInt(req.params.b)));
+})
+app.get('/multiply/:a/:b', function(req, res){
+  res.send(JSON.stringify(parseInt(req.params.a) * parseInt(req.params.b)));
+})
+app.get('/subtract/:a/:b', function(req, res){
+  res.send(JSON.stringify(parseInt(req.params.a) - parseInt(req.params.b)));
+})
+app.get('/divide/:a/:b', function(req, res){
+  res.send(JSON.stringify(parseInt(req.params.a) / parseInt(req.params.b)));
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("listening on port %s", PORT);
+});
